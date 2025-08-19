bafc2d8a4fc30109788ae8523f44d4bc6ffdbe51
nasirali78900 <Yt.automate2025@gmail.com>
2025-08-19 10:48:40 +0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="text-center p-10">
+      <h1 className="text-4xl font-bold text-pink-600">Wedding Invitation</h1>
+      <p className="mt-4 text-lg">Coming Soon...</p>
+    </div>
+  )
+}
